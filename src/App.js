bb0da1bdfd57e38229194e5cb8f5257e842e7067
Anshul <anshulsoni2003@gmail.com>
2023-07-06 10:09:51 +0530

    dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import Navbar from "./components/NavBar";
+import Navbar from "./components/NavBar";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-red">hi</h1>
-      {/* <Navbar/> */}
+   
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className='text-3xl font-bold underlinebg-red'>hola</div>
+  )
+}
+
+export default NavBar
